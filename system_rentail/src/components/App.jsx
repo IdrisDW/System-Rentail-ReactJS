39ee23ad9f00c78871f2e840be6e7347085c2176
YaelHdz <rogelio.yael@outlook.com>
2021-10-17 17:02:49 -0500

    First commit

--- system_rentail/src/components/App.jsx
+++ system_rentail/src/components/App.jsx
@@ -1,13 +1,16 @@
 import '../styles/app.sass'
 import { Header } from './Header'
-import Catalog from './Catalog';
+import Rents from './Rents';
 import { Login } from './Login'
+import { Card } from './Card';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Catalog />
+      <Rents>
+        <Card/>
+      </Rents>
     </div>
   );
 }
---
+++ system_rentail/src/components/Rents.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/rents.sass'
+
+class Rents extends React.Component {
+    render(){
+        return (        
+            <div className="body-rentCatalog">
+        </div>)
+
+    }
+}   
+
+export default Rents;
